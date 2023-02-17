f5e69da19b4833e945f79f0db33f1f26cf3b1e4e
Ayoubhmz <92750888+Ayoubhmz@users.noreply.github.com>
2023-02-17 16:54:40 +0100

    form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductComponent} from "./product/product.component";
+import {OffresEmploiComponentComponent} from "./offres-emploi-component/offres-emploi-component.component";
+import {ArticlesComponentComponent} from "./articles-component/articles-component.component";
 
-const routes: Routes = [];
+import {NotfoundComponent} from "./notfound/notfound.component";
+import {DetailprodComponent} from "./detailprod/detailprod.component";
+import {FormProductComponent} from "./form-product/form-product.component";
+
+const routes: Routes = [
+  {path:'',redirectTo:'Product',pathMatch:"full"},
+  {path:'Product', component:ProductComponent},
+  {path:'Offre', component:OffresEmploiComponentComponent},
+  {path:'Article', component:ArticlesComponentComponent},
+  {path:'detail/:id', component:DetailprodComponent},
+  {path:'Add', component:FormProductComponent},
+
+
+  {path:'**',component:NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import {FormsModule} from "@angular/forms";
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { ArticlesComponentComponent } from './articles-component/articles-component.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { DetailprodComponent } from './detailprod/detailprod.component';
+import { FormProductComponent } from './form-product/form-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    ProductComponent,
+    OffresEmploiComponentComponent,
+    ArticlesComponentComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    DetailprodComponent,
+    FormProductComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
